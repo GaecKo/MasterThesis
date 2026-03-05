cb7bf1b69d77914038efedcc6ef370f06de2e4f7
Eliekhoury17 <93948842+Eliekhoury17@users.noreply.github.com>
2026-03-05 15:35:57 +0100

    Added jwt-auth plugin and serverless-pre-function plugin on all /onboarding routes

--- api_gateway/configure.sh
+++ api_gateway/configure.sh
@@ -74,77 +74,189 @@ success "Route setup"
 info "Test it with: curl http://127.0.0.1:9080/health"
 
 
-# Setup backend addition onboarding route:
+# Gateway admin
+curl -i http://127.0.0.1:9180/apisix/admin/consumers -H 'X-API-KEY: admin' -X PUT -d '
+{
+  "username": "gateway-admin"
+}'
+
+curl -i http://127.0.0.1:9180/apisix/admin/consumers/gateway-admin/credentials -H 'X-API-KEY: admin' -X PUT -d '
+{
+  "id": "cred-gateway-admin",
+  "plugins": {
+    "jwt-auth": {
+      "key": "gateway-admin",
+      "secret": "strong-secret-for-jwt-token-gateway-admin",
+      "algorithm": "HS256"
+    }
+  }
+}'
+
+# Device admin
+curl -i http://127.0.0.1:9180/apisix/admin/consumers \
+  -H 'X-API-KEY: admin' -X PUT -d '
+{
+  "username": "device-admin"
+}'
+
+curl -i http://127.0.0.1:9180/apisix/admin/consumers/device-admin/credentials -H 'X-API-KEY: admin' -X PUT -d '
+{
+  "id": "cred-device-admin",
+  "plugins": {
+    "jwt-auth": {
+      "key": "device-admin",
+      "secret": "strong-secret-for-jwt-token-device-admin",
+      "algorithm": "HS256"
+    }
+  }
+}'
+
+# Backend admin
+curl -i http://127.0.0.1:9180/apisix/admin/consumers -H 'X-API-KEY: admin' -X PUT -d '
+{
+  "username": "backend-admin"
+}'
+
+curl -i http://127.0.0.1:9180/apisix/admin/consumers/backend-admin/credentials -H 'X-API-KEY: admin' -X PUT -d '
+{
+  "id": "cred-backend-admin",
+  "plugins": {
+    "jwt-auth": {
+      "key": "backend-admin",
+      "secret": "strong-secret-for-jwt-token-backend-admin",
+      "algorithm": "HS256"
+    }
+  }
+}'
+
+
+# Setup /onboarding/backend
 info "Setting route /onboarding/backend with Onboarding filter enabled..."
 curl -i http://127.0.0.1:9180/apisix/admin/routes/4 -H 'X-API-KEY: admin' -X PUT -d '
 {
     "uri": "/onboarding/backend",
+    "methods": ["POST", "PATCH", "DELETE"],
     "plugins": {
+        "jwt-auth": {
+            "_meta": {
+                "priority": 13000
+            }
+        },
+        "serverless-pre-function": {
+            "_meta": { "priority": 12500 },
+            "phase": "rewrite",
+            "functions": [
+                "return function(conf, ctx)\n  local consumer = ctx.consumer\n  if not consumer then ngx.status = 401 ngx.say(\"401\") ngx.exit(401) end\n  local allowed = {[\"gateway-admin\"]=true,[\"backend-admin\"]=true}\n  if not allowed[consumer.username] then ngx.status = 403 ngx.say(\"{\\\"message\\\":\\\"Forbidden\\\"}\") ngx.exit(403) end\nend"
+            ]
+        },
         "ext-plugin-pre-req": {
+            "_meta": {
+                "priority": 12000
+            },
             "conf" : [
                 {"name": "Onboarding", "value": "{\"enable\":\"feature\"}"}
             ]
         }
     }
 }'
-
 success "Route setup"
-# Check
 info "Test it with: curl http://127.0.0.1:9080/onboarding/backend"
 
 
-# Setup backend addition onboarding route:
+# Setup /onboarding/backendAuthZ
 info "Setting route /onboarding/backendAuthZ with Onboarding filter enabled..."
 curl -i http://127.0.0.1:9180/apisix/admin/routes/5 -H 'X-API-KEY: admin' -X PUT -d '
 {
     "uri": "/onboarding/backendAuthZ",
+    "methods": ["POST", "PATCH", "DELETE"],
     "plugins": {
+        "jwt-auth": {
+            "_meta": {
+                "priority": 13000
+            }
+        },
+        "serverless-pre-function": {
+            "_meta": { "priority": 12500 },
+            "phase": "rewrite",
+            "functions": [
+                "return function(conf, ctx)\n  local consumer = ctx.consumer\n  if not consumer then ngx.status = 401 ngx.say(\"401\") ngx.exit(401) end\n  local allowed = {[\"gateway-admin\"]=true,[\"backend-admin\"]=true,[\"device-admin\"]=true}\n  if not allowed[consumer.username] then ngx.status = 403 ngx.say(\"{\\\"message\\\":\\\"Forbidden\\\"}\") ngx.exit(403) end\nend"
+            ]
+        },
         "ext-plugin-pre-req": {
+            "_meta": {
+                "priority": 12000
+            },
             "conf" : [
                 {"name": "Onboarding", "value": "{\"enable\":\"feature\"}"}
             ]
         }
     }
 }'
-
 success "Route setup"
-# Check
 info "Test it with: curl http://127.0.0.1:9080/onboarding/backendAuthZ"
 
 
-# Setup backend addition onboarding route:
-info "Setting route /onboarding/backendAuthZ with Onboarding filter enabled..."
+# Setup /onboarding/device
+info "Setting route /onboarding/device with Onboarding filter enabled..."
 curl -i http://127.0.0.1:9180/apisix/admin/routes/6 -H 'X-API-KEY: admin' -X PUT -d '
 {
     "uri": "/onboarding/device",
+    "methods": ["POST", "PATCH", "DELETE"],
     "plugins": {
+        "jwt-auth": {
+            "_meta": {
+                "priority": 13000
+            }
+        },
+        "serverless-pre-function": {
+            "_meta": { "priority": 12500 },
+            "phase": "rewrite",
+            "functions": [
+                "return function(conf, ctx)\n  local consumer = ctx.consumer\n  if not consumer then ngx.status = 401 ngx.say(\"401\") ngx.exit(401) end\n  local allowed = {[\"gateway-admin\"]=true,[\"device-admin\"]=true}\n  if not allowed[consumer.username] then ngx.status = 403 ngx.say(\"{\\\"message\\\":\\\"Forbidden\\\"}\") ngx.exit(403) end\nend"
+            ]
+        },
         "ext-plugin-pre-req": {
+            "_meta": {
+                "priority": 12000
+            },
             "conf" : [
                 {"name": "Onboarding", "value": "{\"enable\":\"feature\"}"}
             ]
         }
     }
 }'
-
 success "Route setup"
-# Check
 info "Test it with: curl http://127.0.0.1:9080/onboarding/device"
 
 
-# Setup backend addition onboarding route:
+# Setup /onboarding/deviceAuthZ
 info "Setting route /onboarding/deviceAuthZ with Onboarding filter enabled..."
 curl -i http://127.0.0.1:9180/apisix/admin/routes/7 -H 'X-API-KEY: admin' -X PUT -d '
 {
     "uri": "/onboarding/deviceAuthZ",
+    "methods": ["POST", "PATCH", "DELETE"],
     "plugins": {
+        "jwt-auth": {
+            "_meta": {
+                "priority": 13000
+            }
+        },
+        "serverless-pre-function": {
+            "_meta": { "priority": 12500 },
+            "phase": "rewrite",
+            "functions": [
+                "return function(conf, ctx)\n  local consumer = ctx.consumer\n  if not consumer then ngx.status = 401 ngx.say(\"401\") ngx.exit(401) end\n  local allowed = {[\"gateway-admin\"]=true,[\"device-admin\"]=true,[\"backend-admin\"]=true}\n  if not allowed[consumer.username] then ngx.status = 403 ngx.say(\"{\\\"message\\\":\\\"Forbidden\\\"}\") ngx.exit(403) end\nend"
+            ]
+        },
         "ext-plugin-pre-req": {
+            "_meta": {
+                "priority": 12000
+            },
             "conf" : [
                 {"name": "Onboarding", "value": "{\"enable\":\"feature\"}"}
             ]
         }
     }
 }'
-
 success "Route setup"
-# Check
 info "Test it with: curl http://127.0.0.1:9080/onboarding/deviceAuthZ"
